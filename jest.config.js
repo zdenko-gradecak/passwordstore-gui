d3a62203287df72fd8d96b21f7edf185bdbfb7d0
Zdenko Gradecak <zdenko.gradecak@gmail.com>
2024-07-22 18:42:27 +0200

    Add Jest for testing

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
