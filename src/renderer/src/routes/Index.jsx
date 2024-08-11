dfcf175226010a5a7eed342b4f4ac1d11880b85a
Zdenko Gradecak <zdenko.gradecak@gmail.com>
2024-08-11 17:54:31 +0200

    Display version

--- src/renderer/src/routes/Index.jsx
+++ src/renderer/src/routes/Index.jsx
@@ -1,15 +1,17 @@
 // import logo from '../assets/logo.png';
+import { version } from '../../../../package.json';
 
 const Index = () => {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
       {/*<img src={logo} alt="Logo" className="w-64 h-64" />*/}
-      <div className="text-4xl pt-6">
+      <div className="text-4xl py-2">
         passwordstore
       </div>
-      <div className="text-4xl pt-2">
+      <div className="text-4xl py-2">
         GUI
       </div>
+      <div className="pt-8">v{version}</div>
     </div>
   );
 };
